756d3e7eb84cfe7631768a7f742491923f4c797d
Yourfavalien <aydenmtz54@gmail.com>
2026-09-08 22:32:39 -0500

    Restore Zilo label on public website

--- xilo-chat.js
+++ xilo-chat.js
@@ -82,7 +82,7 @@
   launcher.type = 'button';
   launcher.setAttribute('aria-expanded', 'false');
   launcher.setAttribute('aria-controls', 'xilo-panel');
-  launcher.innerHTML = '<span class="xilo-launcher-mark" aria-hidden="true">👽</span><span>headquarters assistant</span>';
+  launcher.innerHTML = '<span class="xilo-launcher-mark" aria-hidden="true">👽</span><span>ask Zilo</span>';
 
   const panel = createElement('section', 'xilo-panel');
   panel.id = 'xilo-panel';
